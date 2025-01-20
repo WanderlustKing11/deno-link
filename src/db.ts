6cf244a8eaf55e16c601a4eb30e969efe661c6c1
Wande <wanderlustking11@gmail.com>
2025-01-20 08:42:43 -0500

    Deno KV: Successfully getting and setting data in a backend database (KV)

--- src/db.ts
+++ src/db.ts
@@ -19,3 +19,59 @@ export async function generateShortCode(longURL: string) {
 
     return shortCode;
 }
+
+
+const kv = await Deno.openKv();
+
+export type ShortLink = {
+    shortCode: string;
+    longUrl: string;
+    createdAt: number;
+    userId: string;
+    clickCount: number;
+    lastClickEvent?: string;
+}
+
+export async function storeShortLink(
+    longUrl: string,
+    shortCode: string,
+    userId: string,
+) {
+    const shortLinkKey = ["shortlinks", shortCode];
+    const data: ShortLink = {
+        shortCode,
+        longUrl,
+        userId,
+        createdAt: Date.now(),
+        clickCount: 0,
+    };
+
+    const res = await kv.set(shortLinkKey, data);
+
+    if (!res.ok) {
+        // handle errors
+    }
+
+    return res;
+}
+
+
+export async function getShortLink(shortCode: string) {
+    const link = await kv.get<ShortLink>(["shortlinks", shortCode]);
+    return link.value;
+}
+
+
+// Temporary example to try it out
+// deno run -A --unstable-kv src/db.ts
+const longUrl = 'https://www.github.com';
+const shortCode = await generateShortCode(longUrl)
+const userId = 'test';
+
+console.log(shortCode)
+
+
+await storeShortLink(longUrl, shortCode, userId);
+
+const linkData = await getShortLink(shortCode)
+console.log(linkData)
